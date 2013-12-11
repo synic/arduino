913778637e5d83d85a07687977afbe04e7b88755
Adam Olsen <arolsen@gmail.com>
2013-12-10 21:50:10 -0700

    it actually works now\!

--- simon/src/sketch.ino
+++ simon/src/sketch.ino
@@ -20,10 +20,10 @@
 #include "ButtonEvent.h"
 
 /* LED setup */
-const int LED0 = 0;
-const int LED1 = 1;
-const int LED2 = 2;
-const int LED3 = 3;
+const int LED0 = 12;
+const int LED1 = 11;
+const int LED2 = 10;
+const int LED3 = 9;
 const int ERROR_LED = 7;
 const int LEDS[4] = {LED0, LED1, LED2, LED3};  // the four LED output pins
 
@@ -33,14 +33,14 @@ const int ERROR_NOTE = NOTE_A2;
 const int WON_NOTE = NOTE_A3;
 
 /* Button setup */
-const int BUTTON1 = 8;
-const int BUTTON2 = 9;
-const int BUTTON3 = 10;
-const int BUTTON4 = 11;
+const int BUTTON1 = 5;
+const int BUTTON2 = 2;
+const int BUTTON3 = 6;
+const int BUTTON4 = 4;
 const int BUTTONS[4] = {BUTTON1, BUTTON2, BUTTON3, BUTTON4}; 
 
 /* other */
-const int SPEAKER = 6;
+const int SPEAKER = 3;
 const int MAX_LEVELS = 10;
 
 
@@ -48,16 +48,21 @@ int levelSequence[10] = {0};                // the level sequence
 int level = 1;                              // the current level
 bool inputMode = false;
 int currentStep = -1;
-int noteDuration = 700;
-int pauseDuration = 500;
+int noteDuration = 400;
+int pauseDuration = 200;
+int nextGameMargin = 800;
 bool buttonDown = false;
 
 /**
     Sets up the app
 */
 void setup() {
+    Serial.begin(9600);
     for(int i = 0; i < 4; i++) {
-        ButtonEvent.addButton(LEDS[i], onDown, onUp, NULL, 0, NULL, 0);
+        Serial.println("Adding button:");
+        Serial.println(BUTTONS[i]);
+        ButtonEvent.addButton(BUTTONS[i], onDown, onUp, NULL, 0, NULL, 0);
+        //pinMode(BUTTONS[i], INPUT);
         pinMode(LEDS[i], OUTPUT);
     }
 
@@ -66,21 +71,31 @@ void setup() {
 }
 
 void onDown(ButtonInformation* sender) {
+    Serial.println("onDown() called");
     if(!inputMode || buttonDown) return;
+
+    Serial.println("onDown succeeded");
+
     for(int i = 0; i < 4; i++) {
         if(BUTTONS[i] == sender->pin) {
+            digitalWrite(LEDS[i], HIGH);
             tone(SPEAKER, NOTES_FOR_BUTTON[i]);
+            buttonDown = true;
             return;
         }
     }
 }
 
 void onUp(ButtonInformation* sender) {
+    Serial.println("onUp() called");
     if(!inputMode || !buttonDown) return;
+    Serial.println("onUp succeeded");
     noTone(SPEAKER);
     for(int i = 0; i < 4; i++) {
         if(BUTTONS[i] == sender->pin) {
+            digitalWrite(LEDS[i], LOW);
             buttonPressed(i);
+            buttonDown = false;
             return;
         }
     }
@@ -93,10 +108,14 @@ void setupLevel() {
     currentStep = -1;
     for(int i = 0; i < level; i++) {
         int step = random(0, 4);
+        Serial.write("Level: ");
+        Serial.println(i);
+        Serial.write(": ");
+        Serial.println(step);
         levelSequence[i] = step;
-        digitalWrite(LEDS[i], HIGH);
+        digitalWrite(LEDS[step], HIGH);
         playTone(NOTES_FOR_BUTTON[step], noteDuration, 0);
-        digitalWrite(LEDS[i], LOW);
+        digitalWrite(LEDS[step], LOW);
         delay(pauseDuration);
     }
 }
@@ -106,6 +125,7 @@ void setupLevel() {
 */
 void playTone(int note, long toneDuration, long pauseDuration) {
     tone(SPEAKER, note, noteDuration);
+    delay(noteDuration);
     noTone(SPEAKER);
     if(pauseDuration > 0) delay(pauseDuration);
 }
@@ -114,8 +134,10 @@ void playTone(int note, long toneDuration, long pauseDuration) {
     Plays the game over sound and lights the red error LED
 */
 void playGameOverSound() {
+    Serial.println("Game friggin over.");
     digitalWrite(ERROR_LED, HIGH);
     playTone(ERROR_NOTE, 1000, 500);
+    delay(nextGameMargin);
     digitalWrite(ERROR_LED, LOW);
 }
 
@@ -132,15 +154,25 @@ void playGameWonSound() {
 
 void buttonPressed(int index) {
     currentStep += 1;
+    Serial.println("Button pressed");
+    Serial.println(currentStep);
+    Serial.println(levelSequence[currentStep]);
+    Serial.println(index);
+    Serial.println(level);
     if(index != levelSequence[currentStep]) {
         playGameOverSound();
         inputMode = false;
         level = 1;
+        return;
     }
 
-    if(currentStep >= level) {
+    if(currentStep + 1 >= level) {
         inputMode = false;
         level += 1;
+
+        Serial.println("Continuing to level:");
+        Serial.println(level); 
+        delay(nextGameMargin);
 
         if(level > MAX_LEVELS) {
             playGameWonSound();
@@ -154,7 +186,9 @@ void buttonPressed(int index) {
 /** Main loop **/
 void loop() {
     if(inputMode == false) {
+        Serial.println("Calling setupLevel()");
         setupLevel();
+        Serial.println("Setting inputMode to true");
         inputMode = true;
     }
 
